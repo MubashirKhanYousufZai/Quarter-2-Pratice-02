eb73affb3c837965e258ae342c2396177940ade3
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-09-12 22:46:06 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>HelloWorld
+      .My name is MUBASHIR KHAN YOUSUFZAI
+      .I am new in Web Developing.
+      This is my first project in Next.js.</div>
+    );
+}
